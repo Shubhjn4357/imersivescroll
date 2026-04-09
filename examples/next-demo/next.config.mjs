6e6520cd473332e985aa1df1c66de4b99da2b0ec
Shubhjn4357 <shubhamjain.com.in@gmail.com>
2026-04-09 07:06:39 +0530

    fix alias path

--- examples/next-demo/next.config.mjs
+++ examples/next-demo/next.config.mjs
@@ -13,24 +13,50 @@ const nextConfig = {
     '@immersive-scroll/next',
     '@immersive-scroll/react',
     '@immersive-scroll/core',
-    '@immersive-scroll/shared'
+    '@immersive-scroll/shared',
+    '@immersive-scroll/solid',
+    '@immersive-scroll/web'
   ],
   webpack(config) {
     config.resolve.alias = {
       ...config.resolve.alias,
       'immersive-scroll$': path.resolve(
         currentDirectory,
-        '../../packages/react/src/index.ts'
+        '../../packages/immersive-scroll/src/index.ts'
       ),
       'immersive-scroll/next$': path.resolve(
         currentDirectory,
-        '../../packages/next/src/index.ts'
+        '../../packages/immersive-scroll/src/next.ts'
       ),
       'immersive-scroll/solid$': path.resolve(
         currentDirectory,
-        '../../packages/solid/src/index.ts'
+        '../../packages/immersive-scroll/src/solid.ts'
       ),
       'immersive-scroll/web$': path.resolve(
+        currentDirectory,
+        '../../packages/immersive-scroll/src/web.ts'
+      ),
+      '@immersive-scroll/core$': path.resolve(
+        currentDirectory,
+        '../../packages/core/src/index.ts'
+      ),
+      '@immersive-scroll/next$': path.resolve(
+        currentDirectory,
+        '../../packages/next/src/index.ts'
+      ),
+      '@immersive-scroll/react$': path.resolve(
+        currentDirectory,
+        '../../packages/react/src/index.ts'
+      ),
+      '@immersive-scroll/shared$': path.resolve(
+        currentDirectory,
+        '../../packages/shared/src/index.ts'
+      ),
+      '@immersive-scroll/solid$': path.resolve(
+        currentDirectory,
+        '../../packages/solid/src/index.ts'
+      ),
+      '@immersive-scroll/web$': path.resolve(
         currentDirectory,
         '../../packages/web/src/index.ts'
       )
